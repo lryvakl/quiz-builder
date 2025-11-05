3fb848284bd3dd86cf49e362f841698a744a10d2
Valeriia Ryvak <leraruvak2006@gmail.com>
2025-11-05 22:40:25 +0200

    fix: add root redirect to quizzes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/quizzes',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
